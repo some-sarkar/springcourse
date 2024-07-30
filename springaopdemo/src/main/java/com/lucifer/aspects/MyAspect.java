c3a7722e3011594527584f58680ba601f0db5d8f
some-sarkar <someshwarsarkar9851@gmail.com>
2024-07-30 17:52:16 +0530

    .

--- springaopdemo/src/main/java/com/lucifer/aspects/MyAspect.java
+++ springaopdemo/src/main/java/com/lucifer/aspects/MyAspect.java
@@ -1,20 +1,40 @@
 package com.lucifer.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class MyAspect {
 	
-	@Before("execution(* com.lucifer.payment.MakePayment.doPayment(..))")
-	public void runBefore() {
-		System.out.println("session started");
+	@Pointcut("execution(* com.lucifer.payment.MakePayment.doPayment(..))")
+	public void pointCut() {
+		
 	}
 	
-	@After("execution(* com.lucifer.payment.MakePayment.doPayment(..))")
+	//@Before("execution(* com.lucifer.payment.MakePayment.doPayment(..))")
+	@Before("pointCut()")
+	public void runBefore(JoinPoint joinpoint) {
+		System.out.println("session started before");
+		//System.out.println(joinpoint.toString()); 
+	}
+	
+	//@After("execution(* com.lucifer.payment.MakePayment.doPayment(..))")
+	@After("pointCut()")
 	public void runAfter() {
-		System.out.println("session ended");
+		System.out.println("session ended after");
+	}
+	
+	
+	@Around("pointCut()")
+	public void runAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		System.out.println("session started around");
+		proceedingJoinPoint.proceed();
+		System.out.println("session ended around");
 	}
 	
 }
